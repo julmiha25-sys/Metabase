c0f7e64c79573a62cd8f7ebd00412eaeeafad55b
Юлия Михалева <julmiha25@gmail.com>
2026-01-15 09:11:03 +0500

    Delete Распределение регистраций по компаниям и периоду.sql

--- Распределение регистраций по компаниям и периоду.sql
+++
@@ -1,7 +0,0 @@
-select company.name as Название_компании, count(*) as Количество_регистраций from users 
-join company  on company.id=users.company_id
-where true
-[[and {{company1}}]]
-[[and date_joined between {{date1}} and {{date2}}]]
-group by company.name
-order by company.name
